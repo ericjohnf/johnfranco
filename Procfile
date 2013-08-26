d2ae26cf8c04ca7edf18eef1fa145b63ea3e28dd
Eric Franco <ericjohnf@gmail.com>
2013-08-26 18:54:21 -0400

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn lowercorte:app
---
+++ lowercorte.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    #return render_template('index.html', name=name)
+    return "Four Inches High"
