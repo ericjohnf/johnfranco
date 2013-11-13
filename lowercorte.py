a67a17663b440956194da5a3ea0e34f42751ddbd
Eric Franco <ericjohnf@gmail.com>
2013-11-13 01:46:51 -0500

    Again?

--- lowercorte.py
+++ lowercorte.py
@@ -1,9 +1,31 @@
-import os
+import os, os.path, re
 import sqlite3
+
 
 from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
+
+# helpers
+def tryint(s):
+    try:
+        return int(s)
+    except:
+        return s
+
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [ tryint(c) for c in re.split('([0-9]+)', s) ]  
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False    
+
 
 @app.route('/')
 def hello():
@@ -28,9 +50,26 @@ def drawing():
 @app.route('/painting')
 def painting():
     return render_template('painting.html')
+    
+@app.route('/single')
+@app.route('/single/<date>/<medium>/<file>')
+def single(date=None,medium=None,file=None,names=None):
+    the_path = './static/img/works/'+date+'/'+medium+'/'
+    names = [str(name) for name in os.listdir(the_path)]
+    names.sort(key=alphanum_key)
+    if file in names:
+      names.remove(file)
+    return render_template('single.html',date=date,medium=medium,file=file,names=names)
 
-@app.route('/portfolio')
-@app.route('/portfolio/<date>')
-def portfolio(date=None):
-    return render_template('portfolio.html',date=date)
-
+@app.route('/show')
+@app.route('/show/<date>/<medium>')
+def show(date=None,medium=None,total=None,names=None):
+    the_path = './static/img/works/'+date+'/'+medium+'/'
+    names = [str(name) for name in os.listdir(the_path)]
+    names.sort(key=alphanum_key)
+    total = len([name for name in os.listdir(the_path)])
+    
+    # if date is not None:
+      # split = re.findall(r"[^\W\d_]+|\d+", date)
+      
+    return render_template('show.html',date=date,medium=medium,total=total,names=names)
