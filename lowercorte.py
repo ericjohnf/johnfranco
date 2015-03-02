e8f27b12ae727ad1db6b3088716e95f63e1fc552
Eric Franco <ericjohnf@gmail.com>
2015-03-02 14:08:43 -0500

    Added art to db

--- lowercorte.py
+++ lowercorte.py
@@ -1,11 +1,19 @@
-import os, os.path, re
-import sqlite3
+import os, os.path 
+import sys
+import re
+
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 from werkzeug.serving import run_simple
 
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, Response
+
+art = {'2010': {'paint': [{'medium': 'Oil on Linen', 'date_completed': 2008, 'price': 5760, 'height': 36, 'width': 40, 'id': 1}, {'medium': 'Oil on Linen', 'date_completed': 2007, 'price': 4480, 'height': 40, 'width': 28, 'id': 2}, {'medium': 'Oil on Linen', 'date_completed': 2006, 'price': 4080, 'height': 34, 'width': 30, 'id': 3}, {'medium': 'Oil on Linen', 'date_completed': 2010, 'price': 4608, 'height': 32, 'width': 36, 'id': 4}, {'medium': 'Oil on Linen', 'date_completed': 2010, 'price': 4896, 'height': 34, 'width': 36, 'id': 5}, {'medium': 'Oil on Linen', 'date_completed': 2007, 'price': 5440, 'height': 34, 'width': 40, 'id': 6}, {'medium': 'Oil on Linen', 'date_completed': 2008, 'price': 4352, 'height': 32, 'width': 34, 'id': 7}, {'medium': 'Oil on Linen', 'date_completed': 2007, 'price': 7040, 'height': 44, 'width': 40, 'id': 8}, {'medium': 'Oil on Linen', 'date_completed': 2012, 'price': 8448, 'height': 44, 'width': 48, 'id': 9}, {'medium': 'Oil on Linen', 'date_completed': 2008, 'price': 2000, 'height': 40, 'width': 46, 'id': 10}, {'medium': 'Oil on Linen', 'date_completed': 2012, 'price': 3840, 'height': 30, 'width': 32, 'id': 11}, {'medium': 'Oil on Linen', 'date_completed': 2006, 'price': 4352, 'height': 32, 'width': 34, 'id': 12}, {'medium': 'Oil on Linen', 'date_completed': 2011, 'price': 4896, 'height': 34, 'width': 36, 'id': 13}, {'medium': 'Oil on Linen', 'date_completed': 2011, 'price': 4896, 'height': 34, 'width': 36, 'id': 14}, {'medium': 'Oil on Linen', 'date_completed': 2010, 'price': 3120, 'height': 30, 'width': 26, 'id': 15}, {'medium': 'Oil on Linen', 'date_completed': 2010, 'price': 3840, 'height': 30, 'width': 32, 'id': 16}, {'medium': 'Oil on Linen', 'date_completed': 2004, 'price': 4080, 'height': 34, 'width': 30, 'id': 17}, {'medium': 'Oil on Linen', 'date_completed': 2004, 'price': 3808, 'height': 28, 'width': 34, 'id': 18}, {'medium': 'Oil on Linen', 'date_completed': 2002, 'price': 4080, 'height': 30, 'width': 34, 'id': 19}, {'medium': 'Oil on Linen', 'date_completed': 2010, 'price': 6048, 'height': 36, 'width': 42, 'id': 20}], 'draw': [{'medium': 'Graphite on Bond Paper', 'date_completed': 2010, 'price': 500, 'height': 18, 'width': 27, 'id': 1}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2010, 'price': 500, 'height': 18, 'width': 27, 'id': 2}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2010, 'price': 500, 'height': 18, 'width': 27, 'id': 3}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2010, 'price': 500, 'height': 18, 'width': 27, 'id': 4}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2010, 'price': 500, 'height': 18, 'width': 27, 'id': 5}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2010, 'price': 500, 'height': 18, 'width': 27, 'id': 6}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2010, 'price': 500, 'height': 18, 'width': 27, 'id': 7}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2010, 'price': 500, 'height': 18, 'width': 27, 'id': 8}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2010, 'price': 500, 'height': 18, 'width': 27, 'id': 9}]}, '2013': {'paint': [{'medium': 'Oil on Linen', 'date_completed': 2013, 'price': 2000, 'height': 24, 'width': 20, 'id': 1}, {'medium': 'Oil on Linen', 'date_completed': 2013, 'price': 2000, 'height': 26, 'width': 22, 'id': 2}, {'medium': 'Oil on Linen', 'date_completed': 2013, 'price': 2000, 'height': 26, 'width': 22, 'id': 3}, {'medium': 'Oil on Linen', 'date_completed': 2013, 'price': 2000, 'height': 26, 'width': 22, 'id': 4}, {'medium': 'Oil on Linen', 'date_completed': 2013, 'price': 2000, 'height': 26, 'width': 25, 'id': 5}, {'medium': 'Oil on Linen', 'date_completed': 2013, 'price': 2000, 'height': 24, 'width': 20, 'id': 6}, {'medium': 'Oil on Linen', 'date_completed': 2013, 'price': 2000, 'height': 32, 'width': 30, 'id': 7}, {'medium': 'Oil on Linen', 'date_completed': 2013, 'price': 2000, 'height': 26, 'width': 24, 'id': 8}, {'medium': 'Oil on Linen', 'date_completed': 2013, 'price': 2000, 'height': 32, 'width': 26, 'id': 9}, {'medium': 'Oil on Linen', 'date_completed': 2013, 'price': 2000, 'height': 24, 'width': 27, 'id': 10}], 'draw': [{'medium': 'Graphite on Bond Paper', 'date_completed': 2013, 'price': 500, 'height': 18, 'width': 27, 'id': 1}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2013, 'price': 500, 'height': 18, 'width': 27, 'id': 2}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2012, 'price': 500, 'height': 18, 'width': 27, 'id': 3}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2012, 'price': 500, 'height': 18, 'width': 27, 'id': 4}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2012, 'price': 500, 'height': 18, 'width': 27, 'id': 5}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2012, 'price': 500, 'height': 18, 'width': 27, 'id': 6}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2012, 'price': 500, 'height': 18, 'width': 27, 'id': 7}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2012, 'price': 500, 'height': 18, 'width': 27, 'id': 8}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2012, 'price': 500, 'height': 18, 'width': 27, 'id': 9}, {'medium': 'Graphite on Bond Paper', 'date_completed': 2013, 'price': 500, 'height': 18, 'width': 27, 'id': 10}]}}
+
 
 app = Flask(__name__)
-
+app.engine = create_engine(os.environ['HEROKU_URI'])
+app.debug = True
 # helpers
 def tryint(s):
     try:
@@ -100,6 +108,59 @@ def getNavNames(names,file): # lazy helper cause i'm lazy and dumb derp
       return [names[current_frame-3], names[current_frame-2], names[current_frame-1], names[current_frame+1]]
     else:
       return [names[current_frame-4], names[current_frame-3], names[current_frame-2], names[current_frame-1]]  
+
+@app.route('/art/<id>')
+def art(id):
+    """ Load cover photo onna store by store tip"""
+    query = """SELECT img FROM art WHERE id=:id"""
+    result = app.engine.execute(text(query),id=id).first()
+    mime_type = 'image/jpeg'
+    # Wait does this image even exist?
+    if not result:
+        img = bytes()
+        return Response(img, mimetype=mime_type)    
+    # But does it have a cover photo?
+    if not result['img']:
+        img = bytes()
+    else:
+        img = result['img']
+        # just in case
+        if img[:8] == '\x89\x50\x4E\x47\x0D\x0A\x1A\x0A':
+            mime_type = 'image/png'
+
+    return Response(img, mimetype=mime_type)    
+
+
+def insertArt():
+    for date in art:
+        for medium, pieces in dict.items(art[date]):
+            the_path = './static/img/works/'+date+'/'+medium+'/'
+            dirs = os.listdir(the_path)
+            # names = [str(name) for name in os.listdir(the_path)]
+            for piece in pieces:
+                filename = "./static/img/works/{0}/{1}/{2}.jpg".format(date,medium,piece['id'])
+                print filename
+                with open(filename, "rb") as imageFile:
+                  f = imageFile.read()
+                  bytes = bytearray(f)
+                  # make sure its a jpg
+                  if bytes[:3] == '\xFF\xD8\xFF':
+                      
+                      query = '''INSERT INTO art (title, description, medium, img, show, height, width, year, price, orderid)
+                                 VALUES (:title, :description, :medium, :img, :show, :height, :width, :year, :price, :orderid)'''
+                        
+                      # app.engine.execute(text(query), title='Untitled',
+                      #                                 description='',
+                      #                                 medium=piece['medium'],
+                      #                                 img=bytes,
+                      #                                 show=date,
+                      #                                 height=piece['height'],
+                      #                                 width=piece['width'],
+                      #                                 year=piece['date_completed'],
+                      #                                 price=piece['price'],
+                      #                                 orderid=piece['id'])
+
+    return "Nothing"
 
 if __name__ == '__main__':
     run_simple('localhost', 8000, app, use_reloader=True, use_debugger=True, use_evalex=True) 
